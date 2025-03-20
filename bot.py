71244bffae4f3a8006f46b3e7829c16a84bb1a95
𓆩•𝐊𝐢𝐬𝐬𝐮 💞•𓆪 <kissuXbots@gmail.com>
2025-03-20 07:03:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,9 @@ import asyncio
  
 API_ID = os.environ.get("API_ID","9301087")
 API_HASH = os.environ.get("API_HASH","cbabdb3f23de6326352ef3ac26338d9c")
-BOT_TOKEN = os.environ.get("BOT_TOKEN","7012084014:AAHitJcIDaelpb-9NyU7D_PGkL3u1qnvZeo")
+BOT_TOKEN = os.environ.get("BOT_TOKEN","7879064100:AAFRr4wHNLZaN-LN4E5JYeTi-Qtgc0q1HSc")
 DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://bob:bobfiles1@bob.sp1vv.mongodb.net/?retryWrites=true&w=majority&appName=bob")
-BOT_USERNAME = os.environ.get("BOT_USERNAME","Auto_Deleter_1Bot") # Without @
+BOT_USERNAME = os.environ.get("BOT_USERNAME","autoo_deletee_bot") # Without @
 
 #database
 client = AsyncIOMotorClient(DATABASE_URL)
